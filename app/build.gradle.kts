b6842811b6f0af016d05b36e66157531287fa48f
Zaki Houssam <zakihoussam2006@gmail.com>
2026-07-22 18:46:15 +0100

    Fix Hilt plugin ID in gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.kapt")
-    id("dagger.hilt.android.plugin")
+    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
 plugins {
     // Android Application plugin (applied in the app module)
-    id("com.android.application") version "8.5.0" apply false
+    id("com.android.application") version "8.2.2" apply false
     // Kotlin Android plugin (applied in the app module)
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    // Kotlin Kapt plugin (applied in the app module)
-    id("org.jetbrains.kotlin.kapt") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     // Hilt plugin (applied in the app module)
-    id("dagger.hilt.android.plugin") version "2.50" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 }
 
 // Configure repositories for all sub‑projects
